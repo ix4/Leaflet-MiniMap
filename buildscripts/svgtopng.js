c8db66ed7d0b67da0d4db1b07446ec4ee9f16d28
ix4 <xion@protonmail.com>
2019-11-26 21:36:39 -0500

    x

--- buildscripts/svgtopng.js
+++ buildscripts/svgtopng.js
@@ -2,6 +2,6 @@ var svg2png = require('svg2png');
 var ncp = require('ncp');
 var path = require('path');
 
-svg2png(path.resolve('src/images/toggle.svg'), path.resolve('src/images/toggle.png'), 1.0, function (err) {
-	ncp('src/images/toggle.png', 'dist/images/toggle.png');
+svg2png(path.resolve('./images/toggle.svg'), path.resolve('./images/toggle.png'), 1.0, function (err) {
+	ncp('./images/toggle.png', 'dist/images/toggle.png');
 });
